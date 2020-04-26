ef47ddabcf50f35874e3c320ddd27f59270e0180
Emerson Silva <39895958+silvemerson@users.noreply.github.com>
2020-04-26 14:41:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def verify_token(token):
 @app.route("/")
 @auth.login_required
 def main_route():
-   return "devops test server flying!!"
+   return "Cê é o bixão mesmo!!"
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=80)
